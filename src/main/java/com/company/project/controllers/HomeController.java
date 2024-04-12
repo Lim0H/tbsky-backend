12e8d22182ddbef3c1e3364c5f57f9a6b6f71eab
Lim0H <bikofusyba55@gmail.com>
2024-04-12 20:09:25 +0200

    Fix

--- src/main/java/com/company/project/controllers/HomeController.java
+++ src/main/java/com/company/project/controllers/HomeController.java
@@ -1,11 +1,12 @@
 package com.company.project.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/test")
     public String showHome() {
         return "Hello world!";
     }
